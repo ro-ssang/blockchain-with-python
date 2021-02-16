5ffbeeac9b38dd09de6d11b4611deb5bf28efc58
rossa <fashionfish@naver.com>
2021-02-16 18:20:02 +0900

    Creating a "for" Loop

--- blockchain.py
+++ blockchain.py
@@ -7,6 +7,9 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
+# This function accepts two arguments.
+# One required one (transaction_amount) and one optinal one (last_transaction).
+# The optional one is optional because it has a default value => [1]
 def add_value(transaction_amount, last_transaction=[1]):
     """Append a new value as well as the last blockchain value to the blockchain.
 
@@ -23,13 +26,21 @@ def get_user_input():
     return user_input
 
 
+# Get the first transaction input and add the value to the blockchain
 tx_amount = get_user_input()
 add_value(tx_amount)
 
+# Get the second transaction input and add the value to the blockchain
 tx_amount = get_user_input()
 add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
 
+# Get the third transaction input and add the value to the blockchain
 tx_amount = get_user_input()
 add_value(tx_amount, get_last_blockchain_value())
 
-print(blockchain)
+# Ouput the blockchain list to the console
+for block in blockchain:
+    print("Outputting Block")
+    print(block)
+
+print("Done!")
