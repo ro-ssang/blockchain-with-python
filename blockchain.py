2efc795a90657c9ea17b2e7f8f781f25d059feed
rossa <fashionfish@naver.com>
2021-02-16 19:29:15 +0900

    Improving our Code with Loops & Conditionals

--- blockchain.py
+++ blockchain.py
@@ -4,19 +4,23 @@ blockchain = []
 
 def get_last_blockchain_value():
     """ Returns the last value of the current blockchain. """
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
 # This function accepts two arguments.
 # One required one (transaction_amount) and one optinal one (last_transaction).
 # The optional one is optional because it has a default value => [1]
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
     """Append a new value as well as the last blockchain value to the blockchain.
 
     Arguments:
         :transaction_amount: The amount that should be added.
         :last_transaction: The last blockchain transaction (default [1]).
     """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
@@ -38,10 +42,6 @@ def print_blockchain_elements():
         print(block)
 
 
-# Get the first transaction input and add the value to the blockchain
-tx_amount = get_transaction_value()
-add_value(tx_amount)
-
 while True:
     print("Please choose")
     print("1: Add a new transaction value")
@@ -51,7 +51,7 @@ while True:
 
     if user_choice == "1":
         tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_blockchain_value())
+        add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == "2":
         print_blockchain_elements()
     elif user_choice == "q":
