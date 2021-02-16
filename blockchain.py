29265d725d86d735128113ae0f74b4494abc250b
rossa <fashionfish@naver.com>
2021-02-16 21:59:34 +0900

    Using Conditions inside the "while" Loop

--- blockchain.py
+++ blockchain.py
@@ -58,7 +58,9 @@ def verify_chain():
     return is_valid
 
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print("Please choose")
     print("1: Add a new transaction value")
     print("2: Ouput the blockchain blocks")
@@ -75,7 +77,7 @@ while True:
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     elif user_choice == "q":
-        break
+        waiting_for_input = False
     else:
         print("Input was invalid, please pick a value from the list!")
 
