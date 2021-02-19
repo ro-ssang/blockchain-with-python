85fdc50d70464e99cf184b206bc70b6e2a0cc99c
rossa <fashionfish@naver.com>
2021-02-19 20:58:50 +0900

    Unpacking the Tuple

--- blockchain.py
+++ blockchain.py
@@ -11,7 +11,7 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_transaction(sender, recipient, amount=1.0):
+def add_transaction(recipient, sender=owner, amount=1.0):
     """Append a new value as well as the last blockchain value to the blockchain.
 
     Arguments:
@@ -84,7 +84,9 @@ while waiting_for_input:
 
     if user_choice == "1":
         tx_data = get_transaction_value()
-        add_transaction(tx_amount, get_last_blockchain_value())
+        recipient, amount = tx_data
+        add_transaction(recipient, amount=amount)
+        print(open_transactions)
     elif user_choice == "2":
         print_blockchain_elements()
     elif user_choice == "h":
