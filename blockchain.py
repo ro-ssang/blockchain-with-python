ae08e0cfe2aa0b6bf5c132d7955fc80878830fa4
rossa <fashionfish@naver.com>
2021-02-19 20:45:52 +0900

    Transactions with Dictionaries & Tuples

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,7 @@
 # Initializing our blockchain list
 blockchain = []
+open_transactions = []
+owner = "Rosa"
 
 
 def get_last_blockchain_value():
@@ -9,25 +11,27 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-# This function accepts two arguments.
-# One required one (transaction_amount) and one optinal one (last_transaction).
-# The optional one is optional because it has a default value => [1]
-def add_transaction(transaction_amount, last_transaction=[1]):
+def add_transaction(sender, recipient, amount=1.0):
     """Append a new value as well as the last blockchain value to the blockchain.
 
     Arguments:
-        :transaction_amount: The amount that should be added.
-        :last_transaction: The last blockchain transaction (default [1]).
+        :sender: The sender of the coins.
+        :recipient: The recipient of the coins.
+        :amount: The amount of coins sent with the transaction. (default = 1.0)
     """
-    if last_transaction == None:
-        last_transaction = [1]
-    blockchain.append([last_transaction, transaction_amount])
+    transaction = {"sender": sender, "recipient": recipient, "amount": amount}
+    open_transactions.append(transaction)
+
+
+def mine_block():
+    pass
 
 
 def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float. """
-    user_input = float(input("Your transaction amount please: "))
-    return user_input
+    tx_recipient = input("Enter the recipient of the transaction: ")
+    tx_amount = float(input("Your transaction amount please: "))
+    return tx_recipient, tx_amount
 
 
 def get_user_choice():
@@ -79,7 +83,7 @@ while waiting_for_input:
     user_choice = get_user_choice()
 
     if user_choice == "1":
-        tx_amount = get_transaction_value()
+        tx_data = get_transaction_value()
         add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == "2":
         print_blockchain_elements()
