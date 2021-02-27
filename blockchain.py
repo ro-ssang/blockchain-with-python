2aca58af0890fd37c1b93f61e82a199640abc1ed
rossa <fashionfish@naver.com>
2021-02-27 19:42:56 +0900

    Fixing a Hash Order Fault

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 from functools import reduce
 import hashlib as hl
 import json
+from collections import OrderedDict
 
 MINING_REWARD = 10
 
@@ -12,7 +13,7 @@ participants = {"Rosa"}
 
 
 def hash_block(block):
-    return hl.sha256(json.dumps(block).encode()).hexdigest()
+    return hl.sha256(json.dumps(block, sort_keys=True).encode()).hexdigest()
 
 
 def valid_proof(transactions, last_hash, proof):
@@ -77,7 +78,10 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         :recipient: The recipient of the coins.
         :amount: The amount of coins sent with the transaction. (default = 1.0)
     """
-    transaction = {"sender": sender, "recipient": recipient, "amount": amount}
+    # transaction = {"sender": sender, "recipient": recipient, "amount": amount}
+    transaction = OrderedDict(
+        [("sender", sender), ("recipient", recipient), ("amount", amount)]
+    )
     if verify_transaction(transaction):
         open_transactions.append(transaction)
         participants.add(sender)
@@ -90,11 +94,14 @@ def mine_block():
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
     proof = proof_of_work()
-    reward_transaction = {
-        "sender": "MINING",
-        "recipient": owner,
-        "amount": MINING_REWARD,
-    }
+    # reward_transaction = {
+    #     "sender": "MINING",
+    #     "recipient": owner,
+    #     "amount": MINING_REWARD,
+    # }
+    reward_transaction = OrderedDict(
+        [("sender", "MINING"), ("recipient", owner), ("amount", MINING_REWARD)]
+    )
     copied_transactions = open_transactions[:]
     copied_transactions.append(reward_transaction)
     block = {
