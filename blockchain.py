8e78b960370c4bce65c993b6fe44073cc1611ae2
rossa <fashionfish@naver.com>
2021-02-27 15:52:55 +0900

    Using other Import Syntaxes

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,5 @@
-import functools
-import hashlib
+from functools import reduce
+import hashlib as hl
 import json
 
 MINING_REWARD = 10
@@ -16,7 +16,7 @@ participants = {"Rosa"}
 
 
 def hash_block(block):
-    return hashlib.sha256(json.dumps(block).encode()).hexdigest()
+    return hl.sha256(json.dumps(block).encode()).hexdigest()
 
 
 def get_balance(participant):
@@ -28,7 +28,7 @@ def get_balance(participant):
         tx["amount"] for tx in open_transactions if tx["sender"] == participant
     ]
     tx_sender.append(open_tx_sender)
-    amount_sent = functools.reduce(
+    amount_sent = reduce(
         lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,
         tx_sender,
         0,
@@ -37,7 +37,7 @@ def get_balance(participant):
         [tx["amount"] for tx in block["transactions"] if tx["recipient"] == participant]
         for block in blockchain
     ]
-    amount_received = functools.reduce(
+    amount_received = reduce(
         lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,
         tx_recipient,
         0,
