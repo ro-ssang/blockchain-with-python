c721cbf2f8de508485e10c4c61d777b6b1cae45a
rossa <fashionfish@naver.com>
2021-02-28 13:55:27 +0900

    Understanding Class Attributes

--- oop/car.py
+++ oop/car.py
@@ -1,5 +1,6 @@
 class Car:
     top_speed = 100
+    warnings = []
 
     def drive(self):
         print("I am driving but certainly not faster than {}".format(self.top_speed))
@@ -7,3 +8,14 @@ class Car:
 
 car1 = Car()
 car1.drive()
+
+# Car.top_speed = 200
+car1.warnings.append("New warning")
+
+car2 = Car()
+car2.drive()
+print(car2.warnings)
+
+car3 = Car()
+car3.drive()
+print(car3.warnings)
