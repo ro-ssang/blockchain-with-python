f869ded4ec362600ccca7b2b507ee1e5d90fc38e
rossa <fashionfish@naver.com>
2021-03-01 10:34:24 +0900

    Properties vs Attributes

--- blockchain.py
+++ blockchain.py
@@ -14,13 +14,18 @@ MINING_REWARD = 10
 class Blockchain:
     def __init__(self, hosting_node_id):
         genesis_block = Block(0, "", [], 100, 0)
-        self.__chain = [genesis_block]
+        self.chain = [genesis_block]
         self.__open_transactions = []
         self.load_data()
         self.hosting_node = hosting_node_id
 
-    def get_chain(self):
+    @property
+    def chain(self):
         return self.__chain[:]
+
+    @chain.setter
+    def chain(self, val):
+        self.__chain = val
 
     def get_open_transactions(self):
         return self.__open_transactions[:]
@@ -49,7 +54,7 @@ class Blockchain:
                         block["timestamp"],
                     )
                     updated_blockchain.append(updated_block)
-                self.__chain = updated_blockchain
+                self.chain = updated_blockchain
 
                 open_transactions = json.loads(file_content[1])
                 updated_transactions = []
--- node.py
+++ node.py
@@ -22,7 +22,7 @@ class Node:
 
     def print_blockchain_elements(self):
         # Ouput the blockchain list to the console
-        for block in self.blockchain.get_chain():
+        for block in self.blockchain.chain:
             print("Outputting Block")
             print(block)
         else:
@@ -64,7 +64,7 @@ class Node:
             else:
                 print("Input was invalid, please pick a value from the list!")
 
-            if not Verification.verify_chain(self.blockchain.get_chain()):
+            if not Verification.verify_chain(self.blockchain.chain):
                 self.print_blockchain_elements()
                 print("Invalid blockchain!")
                 break
